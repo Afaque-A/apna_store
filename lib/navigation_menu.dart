7cd42c2771c1914d891fb9b00dad75a893955d29
sohailahmed-dev <Sohailahmed7913@gmail.com>
2025-12-24 04:36:14 +0500

    bottom navigation bar created perfectly

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -16,20 +16,29 @@ class NavigationMenuScreen extends StatelessWidget {
     return Scaffold(
       body: Obx(() => controller.screen[controller.selectedIndex.value]),
       bottomNavigationBar: Obx(
-        () => NavigationBar(
-          elevation: 0,
-          backgroundColor: UColors.light,
-          indicatorColor: UColors.black.withValues(alpha: 0.1),
-          selectedIndex: controller.selectedIndex.value,
-          onDestinationSelected: (int index) {
-            controller.selectedIndex.value = index;
-          },
-          destinations: [
-            NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
-            NavigationDestination(icon: Icon(Iconsax.shop), label: 'Store'),
-            NavigationDestination(icon: Icon(Iconsax.heart), label: 'Wishlist'),
-            NavigationDestination(icon: Icon(Iconsax.user), label: 'Profile'),
-          ],
+        () => ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(50),
+            topRight: Radius.circular(50),
+          ),
+          child: NavigationBar(
+            elevation: 0,
+            backgroundColor: UColors.white,
+            indicatorColor: UColors.black.withValues(alpha: 0.1),
+            selectedIndex: controller.selectedIndex.value,
+            onDestinationSelected: (int index) {
+              controller.selectedIndex.value = index;
+            },
+            destinations: [
+              NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
+              NavigationDestination(icon: Icon(Iconsax.shop), label: 'Store'),
+              NavigationDestination(
+                icon: Icon(Iconsax.heart),
+                label: 'Wishlist',
+              ),
+              NavigationDestination(icon: Icon(Iconsax.user), label: 'Profile'),
+            ],
+          ),
         ),
       ),
     );
